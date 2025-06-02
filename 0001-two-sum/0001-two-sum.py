42f84e249ce4395d7ce6b8a6e284fc8dd7ca782b
minhalmanjee <58848973+minhalmanjee@users.noreply.github.com>
2025-06-02 21:18:53 +0500

    Time: 1 ms (66.21%), Space: 13 MB (90.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for l in range(len(nums)):
-            for r in range(l + 1, len(nums)):  # Ensure r starts after l
-                temp = nums[l] + nums[r]
-                if temp == target:
-                    return [l, r]
-        return []  # Return an empty list if no solution is found
+        prevMap = {} # val: index
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return 
 
                 
         
