1d25ea20a4d85475afd1374d1daabec0adce6d4b
minhalmanjee <58848973+minhalmanjee@users.noreply.github.com>
2025-06-06 17:33:42 +0500

    Time: 12 ms (40.98%), Space: 13.7 MB (31.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,32 @@
 class Solution(object):
     def majorityElement(self, nums):
-        cmap = {}
+        majority = 0
+        vote = 0
+        for num in nums:
+            if vote == 0:
+                vote += 1
+                majority = num
+            elif majority == num:
+                vote +=1
+            else:
+                vote -=1
+        return majority
+
+
+''' cmap = {}
         for num in nums:
             if num in cmap:
                 cmap[num] += 1
             else:
                 cmap[num] = 1
 
-        return(max(cmap, key=cmap.get))
+        return(max(cmap, key=cmap.get)) '''
+
+
+
+
+
+       
 
 
         
