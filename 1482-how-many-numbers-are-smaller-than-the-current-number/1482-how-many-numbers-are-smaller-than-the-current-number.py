9acbecaf68cf9e40a8eed5d93de1263361c3b1b7
minhalmanjee <58848973+minhalmanjee@users.noreply.github.com>
2025-08-03 16:44:03 +0500

    Time: 3 ms (70.89%), Space: 17.8 MB (72.62%) - LeetHub

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        sorted_nums = sorted(nums)
+        hmap = {}
+        for i in range(len(sorted_nums)):
+            if sorted_nums[i] not in hmap:
+                hmap[sorted_nums[i]] = i
+        result = []
+        for num in nums:
+            result.append(hmap[num])
+        return result
+            
+
+
+
+        
