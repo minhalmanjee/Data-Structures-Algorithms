3ffd75d322032b83fb6cc3d3fa0d1a0ae3ab7e37
minhalmanjee <58848973+minhalmanjee@users.noreply.github.com>
2025-11-10 21:39:47 -0700

    Time: 0 ms (100%), Space: 17.7 MB (82.2%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,9 +1,9 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        hmaps={}
+        js = set(jewels)
         i=0
         for char in stones:
-            if char in jewels:
+            if char in js:
                 i += 1
         return i
         
