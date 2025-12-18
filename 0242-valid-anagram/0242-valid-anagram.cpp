98aba66fe3991a2d12034fa3d5e8b9667d881266
Muhammad Minhal Manjee <58848973+minhalmanjee@users.noreply.github.com>
2025-12-17 20:29:53 -0700

    Time: 7 ms (20.08%), Space: 9.8 MB (25.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,44 +1,81 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length())
+        if(s.length() != t.length()){
             return false;
+        }
 
-        quicksort(s, 0, s.length() - 1);
-        quicksort(t, 0, t.length() - 1);
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] != t[i])
+        unordered_map<char, int> mp;
+        for(int i=0; i<s.length(); i++){
+            mp[s[i]]++;
+        }
+        for(int i=0; i<t.length(); i++){
+            mp[t[i]]--;
+            if(mp[t[i]] < 0){
                 return false;
+            }
+
         }
         return true;
+
     }
-int partition(string &s, int l, int r) {
-    char pivot = s[l];
-    int i = l;
-    int j = r;
-
-    while (i < j) {
-        while (i <= j && s[i] <= pivot)
-            i++;
-
-        while (s[j] > pivot)
-            j--;
-
-        if (i < j)
-            swap(s[i], s[j]);
-    }
-
-    swap(s[l], s[j]);
-    return j;
-}
-
-void quicksort(string &s, int l, int r) {
-    if (l < r) {
-        int p = partition(s, l, r);
-        quicksort(s, l, p - 1);
-        quicksort(s, p + 1, r);
-    }
-}
-
 };
+
+
+
+
+//Bruteforce
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+
+//         quicksort(s, 0, s.length()-1);
+//         quicksort(t, 0, t.length()-1);
+
+//         for(int i=0; i<s.length(); i++){
+//             if(s[i] != t[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+//     }
+
+
+
+//     int partition(string &s, int l, int r){
+//         char pivot = s[l];
+//         int i=l;
+//         int j = r;
+//         while(i<j){
+//             while(i<=j && s[i] <= pivot){
+//                 i++;
+//             }
+//             while(s[j] > pivot){
+//                 j--;
+//             }
+//             if(i<j){
+//                 swap(s[i], s[j]);
+//             }
+            
+//         }
+//         swap(s[l], s[j]);
+//         return j;
+
+//     }
+
+//     void quicksort(string &s, int l, int r){
+//         if(l<r){
+//             int p = partition(s,l,r);
+//             quicksort(s, l, p-1);
+//             quicksort(s, p+1, r);
+//         }
+//     }
+// };
+
+
