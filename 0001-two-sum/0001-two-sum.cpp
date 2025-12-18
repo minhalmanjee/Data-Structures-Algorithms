660b6b34efc67d10cbcd142c5f4be3513d13849d
Muhammad Minhal Manjee <58848973+minhalmanjee@users.noreply.github.com>
2025-12-17 21:14:19 -0700

    Time: 43 ms (26.14%), Space: 14.8 MB (37.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,82 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
 
-        for(int i=0; i<nums.size(); i++){
-            for(int j=0; j<nums.size(); j++){
-                if(i==j){
-                    continue;
-                }
-                else{
-                    if(nums[i]+nums[j]==target){
-                        return {i,j};
-                    }
-                }
+        vector<pair<int, int>> arr;
+        for (int i=0; i<n; i++){
+            arr.push_back({nums[i], i});
+        }
+
+        quicksort(arr, 0, n-1);
+
+        int l = 0; int r = n-1;
+        while(l<r){
+            int sum = arr[l].first + arr[r].first;
+            if(sum == target){
+                return {arr[l].second, arr[r].second};
+            }
+            else if(sum < target){
+                l++;
+            }
+            else{
+                r--;
             }
         }
-        return {};
-        
+        return {};        
     }
+
+    void quicksort(vector<pair<int, int>>& arr, int l, int h){
+        if(l<h){
+            int p = partition(arr, l,h);
+            quicksort(arr, l, p-1);
+            quicksort(arr, p+1, h);
+        }
+    }
+
+    int partition(vector<pair<int, int>>& arr, int l, int h){
+        int pivot = arr[l].first;
+        int i = l; int j = h;
+        while(i<j){
+            while(i<=j && arr[i].first <= pivot){
+                i++;
+            }
+            while(arr[j].first > pivot){
+                j--;
+            }
+            if(i<j){
+                swap(arr[i], arr[j]);
+            }
+            
+        }
+        swap(arr[l], arr[j]);
+        return j;
+    }
+
+
 };
+
+
+
+
+//Bruteforce
+//class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+
+//         for(int i=0; i<nums.size(); i++){
+//             for(int j=0; j<nums.size(); j++){
+//                 if(i==j){
+//                     continue;
+//                 }
+//                 else{
+//                     if(nums[i]+nums[j]==target){
+//                         return {i,j};
+//                     }
+//                 }
+//             }
+//         }
+//         return {};
+        
+//     }
+// };
