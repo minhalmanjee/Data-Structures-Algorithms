7b263fa50d282a049d4b06e87df76a25e9257640
Muhammad Minhal Manjee <58848973+minhalmanjee@users.noreply.github.com>
2025-12-22 19:45:19 -0700

    Time: 3 ms (50.4%), Space: 19.8 MB (16.74%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,43 +1,83 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int> values; vector<int> freq;
-        for(int i=0; i<nums.size(); i++){
-            bool found = false;
 
-            for(int j=0; j<values.size(); j++){
-                if(values[j] == nums[i]){
-                    freq[j]++;
-                    found = true;
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++){
+            mp[nums[i]]++;
+        }
+
+        int n = nums.size();  // declare n
+        vector<vector<int>> bucket(n + 1);  // buckets from 0 to n
+
+        for(auto it = mp.begin(); it != mp.end(); it++){
+            bucket[it->second].push_back(it->first);
+        }
+
+        vector<int> res;
+
+        for(int i = n; i >= 0; i--){
+            for(int j = 0; j < bucket[i].size(); j++){
+                res.push_back(bucket[i][j]);
+                if(res.size() == k){
                     break;
                 }
             }
 
-            if(!found){
-                values.push_back(nums[i]);
-                freq.push_back(1);
+            if(res.size() == k){
+                break;
             }
         }
 
+        return res; 
+    }
+};
 
-        vector<int> res;
 
-        for(int i=0; i<k; i++){
-            int maxindex = 0;
 
-            for(int j =0; j<freq.size(); j++){
-                if(freq[j] > freq[maxindex]){
-                    maxindex = j;
-                }
-            }
 
-            res.push_back(values[maxindex]);
-            freq[maxindex] = -1;
-        }
 
-        return res;
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         vector<int> values; vector<int> freq;
+//         for(int i=0; i<nums.size(); i++){
+//             bool found = false;
+
+//             for(int j=0; j<values.size(); j++){
+//                 if(values[j] == nums[i]){
+//                     freq[j]++;
+//                     found = true;
+//                     break;
+//                 }
+//             }
+
+//             if(!found){
+//                 values.push_back(nums[i]);
+//                 freq.push_back(1);
+//             }
+//         }
+
+
+//         vector<int> res;
+
+//         for(int i=0; i<k; i++){
+//             int maxindex = 0;
+
+//             for(int j =0; j<freq.size(); j++){
+//                 if(freq[j] > freq[maxindex]){
+//                     maxindex = j;
+//                 }
+//             }
+
+//             res.push_back(values[maxindex]);
+//             freq[maxindex] = -1;
+//         }
+
+//         return res;
 
 
         
-    }
-};
+//     }
+// };
