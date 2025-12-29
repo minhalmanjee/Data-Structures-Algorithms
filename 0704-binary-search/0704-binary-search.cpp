ecae555151177689b453edde69293c34003d97c6
Muhammad Minhal Manjee <58848973+minhalmanjee@users.noreply.github.com>
2025-12-28 20:51:12 -0700

    Time: 0 ms (100%), Space: 31.3 MB (79.07%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,24 @@
+// Iterative BS
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s=0; int e=nums.size()-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(target < nums[mid]){
+                e=mid-1;
+            }
+            else if(target > nums[mid]){
+                s=mid+1;
+            }
+            else if(target == nums[mid]){
+                return mid;
+            }
+
+        }
+        return -1;
+
+
+        
+    }
+};
