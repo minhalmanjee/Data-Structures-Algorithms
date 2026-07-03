e096b544968291f024fb494e00fb01feeb400cb5
Muhammad Minhal Manjee <58848973+minhalmanjee@users.noreply.github.com>
2026-07-02 21:48:34 -0600

    Time: 0 ms (100%), Space: 13 MB (81.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,36 @@
-#Time: O(nlogn)
-#Space: O(n)
+#Time: O(1) hashmap + O(n)
+#Space: O(n) for hashmap
+
 class Solution(object):
     def twoSum(self, nums, target):
-        newnums = sorted((num,i) for i, num in enumerate(nums)) # sorting: O(nlogn)
-        left, right = 0, len(newnums) - 1
-        while left < right:                                     # traverse: O(n)
-            cur_sum = newnums[left][0] + newnums[right][0]
-            if cur_sum == target:
-                return [newnums[left][1], newnums[right][1]]
-            elif cur_sum < target:
-                left += 1
-            else:
-                right -= 1
+        prevmap = {} # val : index
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in prevmap:
+                return [prevmap[diff], i]
+            prevmap[n] = i
+        return
+            
+
+
+
+
+
+
+# #Time: O(nlogn)
+# #Space: O(n)
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         newnums = sorted((num,i) for i, num in enumerate(nums)) # sorting: O(nlogn)
+#         left, right = 0, len(newnums) - 1
+#         while left < right:                                     # traverse: O(n)
+#             cur_sum = newnums[left][0] + newnums[right][0]
+#             if cur_sum == target:
+#                 return [newnums[left][1], newnums[right][1]]
+#             elif cur_sum < target:
+#                 left += 1
+#             else:
+#                 right -= 1
 
 
 
