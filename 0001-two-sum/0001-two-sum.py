c66b04d9a87eb5990b07d2351a7157214cf07e00
Muhammad Minhal Manjee <58848973+minhalmanjee@users.noreply.github.com>
2026-07-02 21:09:18 -0600

    Time: 7 ms (45.89%), Space: 13.5 MB (16.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,27 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for a in range(len(nums)-1):
-            for b in range(a+1, len(nums)):
-                if nums[a] + nums[b] == target:
-                    return [a,b]
+        newnums = sorted((num,i) for i, num in enumerate(nums))
+        left, right = 0, len(newnums) - 1
+        while left < right:
+            cur_sum = newnums[left][0] + newnums[right][0]
+            if cur_sum == target:
+                return [newnums[left][1], newnums[right][1]]
+            elif cur_sum < target:
+                left += 1
+            else:
+                right -= 1
+
+
+    
+
+        
+
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         for a in range(len(nums)-1):
+#             for b in range(a+1, len(nums)):
+#                 if nums[a] + nums[b] == target:
+#                     return [a,b]
 
         
