49ca3c39b2bd63f114ba76f07a932b44b1566250
Muhammad Minhal Manjee <58848973+minhalmanjee@users.noreply.github.com>
2026-07-02 21:10:22 -0600

    Time: 29 ms (44.84%), Space: 13.5 MB (16.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,10 @@
+#Time: O(nlogn)
+#Space: O(n)
 class Solution(object):
     def twoSum(self, nums, target):
-        newnums = sorted((num,i) for i, num in enumerate(nums))
+        newnums = sorted((num,i) for i, num in enumerate(nums)) # sorting: O(nlogn)
         left, right = 0, len(newnums) - 1
-        while left < right:
+        while left < right:                                     # traverse: O(n)
             cur_sum = newnums[left][0] + newnums[right][0]
             if cur_sum == target:
                 return [newnums[left][1], newnums[right][1]]
@@ -10,6 +12,8 @@ class Solution(object):
                 left += 1
             else:
                 right -= 1
+
+
 
 
     
